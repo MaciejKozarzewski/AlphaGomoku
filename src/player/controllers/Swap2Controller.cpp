4801411a101d6d49dc917d8b233a287eac5adc56
MaciejKozarzewski <mk@eskrzynka.pl>
2022-04-08 14:34:11 +0200

    Added logging.

--- src/player/controllers/Swap2Controller.cpp
+++ src/player/controllers/Swap2Controller.cpp
@@ -8,6 +8,7 @@
 #include <alphagomoku/player/controllers/Swap2Controller.hpp>
 #include <alphagomoku/player/TimeManager.hpp>
 #include <alphagomoku/player/SearchEngine.hpp>
+#include <alphagomoku/utils/Logger.hpp>
 
 namespace ag
 {
@@ -25,13 +26,16 @@ namespace ag
 			switch (Board::numberOfMoves(search_engine.getBoard()))
 			{
 				case 0:
+					Logger::write("Placing first 3 stones");
 					state = ControllerState::PUT_FIRST_3_STONES;
 					break;
 				case 3:
+					Logger::write("Evaluating first 3 stones");
 					state = ControllerState::EVALUATE_FIRST_3_STONES;
 					start_search(false);
 					break;
 				case 5:
+					Logger::write("Evaluating first 5 stones");
 					state = ControllerState::EVALUATE_5_STONES;
 					start_search(false);
 					break;
@@ -52,14 +56,16 @@ namespace ag
 			{
 				stop_search();
 				const SearchSummary summary = search_engine.getSummary( { }, false);
-				if (summary.node.getValue().getExpectation() < 0.333f)
+				const float expectation_value = summary.node.getValue().getExpectation();
+				Logger::write("Evaluated 3 stone opening : " + std::to_string(100 * expectation_value) + "% probability of winning");
+				if (expectation_value < 0.333f)
 				{
 					outputQueue.push(Message(MessageType::BEST_MOVE, "swap"));
 					state = ControllerState::IDLE;
 				}
 				else
 				{
-					if (summary.node.getValue().getExpectation() > 0.666f)
+					if (expectation_value > 0.666f)
 					{
 						BestEdgeSelector selector;
 						const Move best_move = selector.select(&summary.node)->getMove();
@@ -68,7 +74,9 @@ namespace ag
 					}
 					else
 					{
+						Logger::write("Balancing 3 stone position");
 						first_balancing_move = get_balancing_move();
+						Logger::write("First balancing move : " + first_balancing_move.toString() + " (" + first_balancing_move.text() + ")");
 
 						// update board and set up the search with new board
 						matrix<Sign> board = search_engine.getBoard();
@@ -77,6 +85,7 @@ namespace ag
 
 						// start search to find second balancing move
 						start_search(true);
+
 						state = ControllerState::BALANCE_THE_OPENING;
 					}
 				}
@@ -91,6 +100,7 @@ namespace ag
 			{
 				stop_search();
 				second_balancing_move = get_balancing_move();
+				Logger::write("Second balancing move : " + second_balancing_move.toString() + " (" + second_balancing_move.text() + ")");
 
 				std::vector<Move> response = { first_balancing_move, second_balancing_move };
 				outputQueue.push(Message(MessageType::BEST_MOVE, response));
@@ -148,6 +158,7 @@ namespace ag
 		const SearchSummary summary = search_engine.getSummary( { }, false);
 		const int depth = Board::numberOfMoves(search_engine.getBoard());
 		BalancedSelector selector(depth + 1, PuctSelector(0.0f));
+		Logger::write(summary.node.toString());
 		return selector.select(&summary.node)->getMove();
 	}
 
