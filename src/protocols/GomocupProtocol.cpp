da9ef13b73db4021142faaaaca4362b8459cd2eb
MaciejKozarzewski <mk@eskrzynka.pl>
2023-11-08 16:02:15 +0100

    Revert "Fixed redundant empty evaluation lines."
    This reverts commit 4b48d923f33dee47617ce293a6462afada7881b2.

--- src/protocols/GomocupProtocol.cpp
+++ src/protocols/GomocupProtocol.cpp
@@ -267,7 +267,7 @@ namespace ag
 		const Message msg = output_queue.pop();
 		if (msg.holdsString())
 			sender.send("MESSAGE " + msg.getString());
-		if (msg.holdsSearchSummary() and msg.getSearchSummary().node.getVisits() > 0)
+		if (msg.holdsSearchSummary())
 			sender.send("MESSAGE " + parse_search_summary(msg.getSearchSummary()));
 	}
 	void GomocupProtocol::about_engine(OutputSender &sender)
