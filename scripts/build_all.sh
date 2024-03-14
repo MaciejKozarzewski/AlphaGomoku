29e4bb53e06834587a497897fb45b604c3ed5510
MaciejKozarzewski <mk@eskrzynka.pl>
2024-03-14 20:24:54 +0100

    Improved build system.

---
+++ scripts/build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ..
+echo "$PWD"
+mkdir build
+cmake -B build/Release -DCMAKE_BUILD_TYPE=Release -DBUILD_WITH_CUDA=OFF -DBUILD_WITH_OPENCL=OFF
+cmake --build build/Release
+cmake -B build/Release -DCMAKE_BUILD_TYPE=Release -DBUILD_WITH_CUDA=ON -DBUILD_WITH_OPENCL=OFF
+cmake --build build/Release
+cmake -B build/Release -DCMAKE_BUILD_TYPE=Release -DBUILD_WITH_CUDA=OFF -DBUILD_WITH_OPENCL=ON
+cmake --build build/Release
+
+cmake -B build/Debug -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_CUDA=OFF -DBUILD_WITH_OPENCL=OFF
+cmake --build build/Debug
+cmake -B build/Debug -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_CUDA=ON -DBUILD_WITH_OPENCL=OFF
+cmake --build build/Debug
+cmake -B build/Debug -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_CUDA=OFF -DBUILD_WITH_OPENCL=ON
+cmake --build build/Debug
